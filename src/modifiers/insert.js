6223c7b3cd8f8c8ed604ea628fa193048ef6853f
Samuel Tobia <sam@system76-pc.localdomain>
2022-01-13 13:11:18 -0800

    Code Cleanup: both insertAny and insertUnique use the sa e insertItemAtIndex

--- src/modifiers/insert.js
+++ src/modifiers/insert.js
@@ -7,36 +7,32 @@ import { findAny } from "../search";
 export function insertItemUnique(array, item, compare){
   var index = findAny(array, item, compare);
   if(isFound(index)) throw new Error(cannotInsertAtExistingItem);
-  if(index === Number.POSITIVE_INFINITY){
-    return (array).concat([item]);
-  }
-  if(index === Number.NEGATIVE_INFINITY){
-    return ([item]).concat(array);
-  }
   index = -1 * index;
-  return (array.slice(0, index)).concat([item]).concat(index);
+  return insertItemAtIndex(array, item, index);
 }
-
-export function insertItemAny(array, item, compare){
-  var index = findAny(array, item, compare);
-  if(index === Number.POSITIVE_INFINITY){
-    return (array).concat([item]);
-  }
-  if(index === Number.NEGATIVE_INFINITY){
-    return ([item]).concat(array);
-  }
-  index = prepareIndexForUse(index);
-  return (array.slice(0, index)).concat([item]).concat(array.slice(index));
-}
-
 export function insertAllUnique(sortedArray, unsortedArray, compare){
   return unsortedArray.reduce((array, item)=>{
     return insertItemUnique(array, item, compare);
   }, sortedArray);
 }
 
+export function insertItemAny(array, item, compare){
+  var index = findAny(array, item, compare);
+  index = prepareIndexForUse(index);
+  return insertItemAtIndex(array, item, index);
+}
 export function insertAllAny(sortedArray, unsortedArray, compare){
   return unsortedArray.reduce((array, item)=>{
     return insertItemAny(array, item, compare);
   }, sortedArray);
+}
+
+export function insertItemAtIndex(array, item, index){
+  if(index >= Number.POSITIVE_INFINITY){
+    return (array).concat([item]);
+  }
+  if(index === Number.NEGATIVE_INFINITY){
+    return ([item]).concat(array);
+  }
+  return (array.slice(0, index)).concat([item]).concat(index);
 }
