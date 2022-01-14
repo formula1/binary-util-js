1512d206f8f18de46466b1197b1b7a8df2ab0528
Samuel Tobia <sam@system76-pc.localdomain>
2022-01-13 22:09:30 -0800

    Test Feature: added an insert test

---
+++ tests/modifiers/insert.js
@@ -0,0 +1,34 @@
+
+import { isArraySorted } from "../utility.js";
+import { insertItemUnique } from "../../src/modifiers/insert.js";
+
+var sortedArray = "abcdefghijklmnopqrstuvwxyz".split("");
+
+function runTest(name, fn){
+  const start = Date.now();
+  var fnResult = fn();
+  console.log(name, "result:", fnResult, Date.now() - start);
+}
+
+function strCompare(a, b){
+  return (a).localeCompare(b);
+}
+
+var counter = 0;
+while(counter < sortedArray.length){
+  runTest("findAny", ()=>{
+    var arrayWithMissing = [...sortedArray];
+
+    // const index = Math.floor(Math.random() * arrayWithMissing.length);
+    var item = arrayWithMissing.splice(counter, 1)[0];
+    var completeArray = insertItemUnique(arrayWithMissing, item, strCompare);
+    return {
+      index: counter,
+      item: item,
+      arrayWithMissing: arrayWithMissing.join(""),
+      arrayWithItem: completeArray.join(""),
+      isArraySorted: isArraySorted(completeArray, strCompare)
+    };
+  });
+  counter++;
+}
