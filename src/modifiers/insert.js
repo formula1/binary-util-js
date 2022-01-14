e9f8dbf0db72cbacac5e32e984dca0b9e68c3cd9
Samuel Tobia <sam@system76-pc.localdomain>
2022-01-13 21:44:42 -0800

    Code Fix: was reversing negative infinity

--- src/modifiers/insert.js
+++ src/modifiers/insert.js
@@ -1,13 +1,16 @@
 
 import { cannotInsertAtExistingItem } from "../errors.js";
 
-import { isFound, prepareIndexForUse } from "../utility,js";
+import { isFound, prepareIndexForUse } from "../utility.js";
 import { findAny } from "../search/findAny.js";
 
 export function insertItemUnique(array, item, compare){
   var index = findAny(array, item, compare);
+  console.log(index);
   if(isFound(index)) throw new Error(cannotInsertAtExistingItem);
-  index = -1 * index;
+  if(Math.abs(index) !== Number.POSITIVE_INFINITY){
+    index = -1 * index;
+  }
   return insertItemAtIndex(array, item, index);
 }
 export function insertAllUnique(sortedArray, unsortedArray, compare){
@@ -28,11 +31,17 @@ export function insertAllAny(sortedArray, unsortedArray, compare){
 }
 
 export function insertItemAtIndex(array, item, index){
-  if(index >= Number.POSITIVE_INFINITY){
+  if(index === Number.POSITIVE_INFINITY){
+    console.log("adding to the end")
     return (array).concat([item]);
   }
   if(index === Number.NEGATIVE_INFINITY){
+    console.log("adding to the start")
     return ([item]).concat(array);
   }
-  return (array.slice(0, index)).concat([item]).concat(index);
+  return (
+    (array.slice(0, index))
+    .concat([item])
+    .concat(array.slice(index))
+  );
 }
